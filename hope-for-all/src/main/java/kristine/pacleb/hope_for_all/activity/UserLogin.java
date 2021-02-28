b1e9caa3324440e65eebb070b42936884d08bce1
navjotsengh <72287772+navjotsengh@users.noreply.github.com>
2021-02-28 00:41:02 -0500

    User activity created

---
+++ hope-for-all/src/main/java/kristine/pacleb/hope_for_all/activity/UserLogin.java
@@ -0,0 +1,13 @@
+package kristine.pacleb.hope_for_all.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class UserLogin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
